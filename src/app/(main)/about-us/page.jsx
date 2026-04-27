d371fd98ff243826fcc52ae7d75cbd7f3e9b5103
Mamun123123123 <almalm90459@gmail.com>
2026-04-27 09:40:01 +0600

    Design Navbar

---
+++ src/app/(main)/about-us/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const aboutus = () => {
+  return (
+    <div>
+      About
+    </div>
+  )
+}
+
+export default aboutus
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
-
+import userAvater from '@/assets/user.png'
+import Navlink from './Navlink'
 const Navbar = () => {
   return (
-    <div>
-      navbar
+    <div className='flex p-2 justify-between container mx-auto'>
+      <div>
+
+      </div>
+      <ul className='flex gap-4'>
+        <li><Navlink href={'/'}>Home</Navlink></li>
+        <li><Navlink href={'/about-us'}>About</Navlink></li>
+        <li><Navlink href={'/career'}>Career</Navlink></li>
+      </ul>
+      <div className='flex gap-1'>
+        <Image src={userAvater} alt="user" width={40} height={10}/>
+        <button className='btn btn-primary text-black'><Link href={"/login"}>Login</Link></button>
+      </div>
     </div>
   )
 }
