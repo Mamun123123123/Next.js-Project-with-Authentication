e82e3a9408c9e33312be1832ef38a465227260e8
Mamun123123123 <almalm90459@gmail.com>
2026-04-27 11:12:24 +0600

    Display Categories & Design Sidebar

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,3 +1,5 @@
+import LeftSidebar from "@/components/homepage/news/LeftSidebar";
+import RightSidebar from "@/components/homepage/RightSidebar";
 import Image from "next/image";
 
 async function getCategories() {
@@ -12,16 +14,11 @@ export default async function Home() {
   return (
    <div className="grid items-center text-center  grid-cols-12 gap-4">
     <div className="col-span-3 m-2">
-      All categories
-      <ul className="flex flex-col gap-3 p-2"> {
-        categories.news_category.map((news) => {
-          return <li key={news.category_id} className="bg-slate-100 p-2 text-black rounded-2xl">{news.category_name}</li>
-        })
-      }</ul>
+       <LeftSidebar categories={categories} activeId={null}/>
      
       </div>
     <div className="col-span-6 bg-yellow-950">All News</div>
-    <div className="col-span-3 bg-blue-950">Social icons</div>
+    <div className="col-span-3 bg-blue-950"><RightSidebar /></div>
    </div>
   );
 }
