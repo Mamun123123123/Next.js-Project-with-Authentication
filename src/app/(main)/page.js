b17472f0f64161a44d1bda4c4a7e9267ca5137c5
Mamun123123123 <almalm90459@gmail.com>
2026-04-27 10:43:06 +0600

    Design Grid Layout, Fetch Categories

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,7 +1,27 @@
 import Image from "next/image";
 
-export default function Home() {
+async function getCategories() {
+  const res = await fetch("https://openapi.programming-hero.com/api/news/categories")
+  const data = await res.json()
+  return data.data  
+}
+
+
+export default async function Home() {
+  const categories = await getCategories()
   return (
-   <h1>Home Page</h1>
+   <div className="grid items-center text-center  grid-cols-12 gap-4">
+    <div className="col-span-3 m-2">
+      All categories
+      <ul className="flex flex-col gap-3 p-2"> {
+        categories.news_category.map((news) => {
+          return <li key={news.category_id} className="bg-slate-100 p-2 text-black rounded-2xl">{news.category_name}</li>
+        })
+      }</ul>
+     
+      </div>
+    <div className="col-span-6 bg-yellow-950">All News</div>
+    <div className="col-span-3 bg-blue-950">Social icons</div>
+   </div>
   );
 }
