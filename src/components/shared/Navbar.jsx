79bbe672faccd13bba4a76bea92a5b3d061ecb83
Mamun123123123 <almalm90459@gmail.com>
2026-04-28 14:38:38 +0600

    Logged In User, Signout, Password Toggle

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,9 +1,13 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import userAvater from '@/assets/user.png'
 import Navlink from './Navlink'
+import { authClient } from '@/lib/auth-client'
 const Navbar = () => {
+  const { data: session ,isPending } = authClient.useSession()
+  const user = session?.user;
   return (
     <div className='flex p-2 justify-between container mx-auto'>
       <div>
@@ -14,10 +18,15 @@ const Navbar = () => {
         <li><Navlink href={'/about-us'}>About</Navlink></li>
         <li><Navlink href={'/career'}>Career</Navlink></li>
       </ul>
-      <div className='flex gap-1'>
-        <Image src={userAvater} alt="user" width={40} height={10}/>
+      {isPending ? <span className="loading loading-spinner loading-lg"></span> :  user ? ( <div className='flex gap-1 items-center'>
+        <h2>{user.name}</h2>
+        <Image src={user.image || userAvater} alt="user" width={40} height={10}/>
+        <button className='btn btn-primary text-black' onClick={async()=>await authClient.signOut()}>Logout</button>
+      </div>) : (
         <button className='btn btn-primary text-black'><Link href={"/login"}>Login</Link></button>
-      </div>
+
+       )}
+     
     </div>
   )
 }
